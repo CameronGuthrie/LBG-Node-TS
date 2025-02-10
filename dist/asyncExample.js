68db11f907bfe18e2083c0aaf357864548a4a7a8
CameronGuthrie <CGuthrie@qa.com>
2025-02-10 11:15:49 +0000

    async example

---
+++ dist/asyncExample.js
@@ -0,0 +1,7 @@
+"use strict";
+async function fetchData() {
+    console.log("Fetching data...");
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log("Data retrieved!");
+}
+fetchData();
